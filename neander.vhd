e87bc7ee8c0b56c51c8c16dc4285d1a7f334a8ac
Gabriel Fredes <gabriel.sfredes@gmail.com>
2023-12-10 00:29:14 -0300

    updt: neander.vhd
    testado no modelsim, removido função de leitura de arquivo e posto memória "manual"

--- neander.vhd
+++ neander.vhd
@@ -8,29 +8,37 @@ ENTITY neander IS
     PORT (
         reset : IN STD_LOGIC;
         clk : IN STD_LOGIC;
+        count_load : IN STD_LOGIC;
+        en_ULA : IN STD_LOGIC;
         -- saidas
         Z : OUT STD_LOGIC;
         N : OUT STD_LOGIC;
-        decoder : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-    )
+        decoder : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+    );
 END neander;
 
-ARCHITECTURE behavior OF neanderis
+ARCHITECTURE behavior OF neander IS
     -- memoria do neander
-    TYPE valores IS ARRAY (0 TO 15) OF STD_LOGIC_VECTOR(7 DOWNTO 0); -- 16 posicoes de 8 bits
+    TYPE valores IS ARRAY (0 TO 15) OF STD_LOGIC_VECTOR(7 DOWNTO 0);--vetor de 16 posi��es de 8 bits
 
-    -- leitura do arquivo com a memoria a ser lida e trabalhada
-    SIGNAL memoria : valores;
-    FILE arquivo : TEXT;
-    VARIABLE linha : LINE;
-    file_open(arquivo, "memoria.txt", READ_MODE);
-
-    FOR i IN 0 TO 15 LOOP
-        readline(arquivo, linha);
-        READ(linha, memoria(i));
-    END LOOP;
-
-    file_close(arquivo);
+    -- alterar aqui a memória, infelizmente não consegui implementar com leitura do arquivo
+    SIGNAL memoria : valores := (0 => "10100001",
+    1 => "10100001",
+    2 => "00000000",
+    3 => "00000000",
+    4 => "00000000",
+    5 => "00000000",
+    6 => "00000000",
+    7 => "00000000",
+    8 => "00000000",
+    9 => "00000000",
+    10 => "00000000",
+    11 => "00000000",
+    12 => "00000000",
+    13 => "00000000",
+    14 => "00000000",
+    15 => "00000000"
+    );
 
     ----------------------------------------------
 
@@ -59,9 +67,9 @@ BEGIN
     -- mux
     -- o uso dos bits 4 e 5 do RDM é para indicar qual operação será feita, conforme visualizado no logisim
     MUX <= "0000" & RDM(3 DOWNTO 0) WHEN (RDM(5) = '0' AND RDM(4) = '0') ELSE -- load
-        "0000" & SUM(3 DOWNTO 0) WHEN (RDM(5) = '0' AND RDM(4) = '1') ELSE -- soma
+        "0000" & SOMA(3 DOWNTO 0) WHEN (RDM(5) = '0' AND RDM(4) = '1') ELSE -- soma
         "0000" & SUB(3 DOWNTO 0) WHEN (RDM(5) = '1' AND RDM(4) = '0') ELSE -- subtração
-        "0000" & mult(7 DOWNTO 4) WHEN (RDM(5) = '1' AND RDM(4) = '1'); -- multiplicaação
+        "0000" & MULT(7 DOWNTO 4) WHEN (RDM(5) = '1' AND RDM(4) = '1'); -- multiplicaação
 
     -- flags
     flagZ <= '1' WHEN (ACC = "0000") ELSE
@@ -79,19 +87,19 @@ BEGIN
             Z <= '0';
             N <= '0';
             decoder <= (OTHERS => '0');
+
         ELSIF rising_edge(clk) THEN
             IF count_load = '1' THEN
                 PC <= PC + 1;
-                RDM <= memoria(to_integer(unsigned(PC)));
-                decoder <= RDM;
-                (7 DOWNTO 4); -- os 4 bits mais significativos do RDM são o decoder
+                RDM <= memoria(conv_integer(unsigned(PC)));
+                decoder <= RDM(7 DOWNTO 4); -- os 4 bits mais significativos do RDM são o decoder
             ELSE
                 PC <= RDM(3 DOWNTO 0); -- os 4 bits menos significativos do RDM são o PC
-                RDM <= memoria(to_integer(unsigned(PC)));
+                RDM <= memoria(conv_integer(unsigned(PC)));
                 decoder <= RDM(7 DOWNTO 4);
             END IF;
             IF en_ULA = '1' THEN
-                AXX <= MUX(3 DOWNTO 0);
+                ACC <= MUX(3 DOWNTO 0);
             END IF;
         END IF;
     END PROCESS;
